9650a2b8a259a3327423f9486b8b9f787f9d75de
Ichoku Chinonso <greatchinonso7@gmail.com>
2021-03-20 21:54:29 +0100

    WIP Auth Input components

--- src/shared/auth-input/index.js
+++ src/shared/auth-input/index.js
@@ -4,7 +4,7 @@ import { images } from '../../images';
 import TouchItem from '../touch-Item';
 import { styles } from './style';
 import { wp } from '../resposive-dimension';
-import PropTypes from 'prop-types';
+import IoniconsIcon from 'react-native-vector-icons/Ionicons';
 const { icons } = images;
 
 const AuthInput: React.FC<TextInputProps> = React.forwardRef(
@@ -28,7 +28,29 @@ const AuthInput: React.FC<TextInputProps> = React.forwardRef(
       <TouchItem
         onPress={() => textInputRef.current.focus()}
         style={[styles.inputContainer, containerStyle]}>
-        <TextInput />
+        {showLabel && <Text style={styles.inputLabel}>{label}</Text>}
+        <TextInput
+          value={value}
+          secureTextEntry={isPasswordVisible}
+          style={[
+            styles.inputBox,
+            showLabel && { width: '70%' },
+            hasRightIcon && { width: wp(180) },
+          ]}
+          ref={textInputRef}
+          {...rest}
+        />
+        {hasRightIcon && (
+          <TouchItem
+            onPress={() => toggleVisibility(!isPasswordVisible)}
+            style={styles.rightIconContainer}>
+            <Image
+              source={
+                isPasswordVisible ? icons.eyeClose : <IoniconsIcon name="eye" />
+              }
+            />
+          </TouchItem>
+        )}
       </TouchItem>
     );
   },
