bba1864691d5f4a1169fb7941c83171a1edc9689
Ichoku Chinonso <greatchinonso7@gmail.com>
2021-03-20 20:39:57 +0100

    initial commit

---
+++ env.dev.js
@@ -0,0 +1,16 @@
+export default {
+  name: 'dev',
+  baseUrl: 'https://manator-api-br-rn-challenge.herokuapp.com',
+  defaultSignup: {
+    email: '',
+    password: '',
+    firstName: '',
+    lastName: '',
+    tradingName: '',
+    phone: '',
+    address: '',
+    businessDescription: '',
+  },
+  demo: true,
+  version: '1.0.0',
+};
---
+++ src/shared/auth-input/index.js
@@ -0,0 +1,2 @@
+import React, { useState, useRef } from 'react';
+import { TextInput, TextInputProps, Text, Image, View } from 'react-native';
